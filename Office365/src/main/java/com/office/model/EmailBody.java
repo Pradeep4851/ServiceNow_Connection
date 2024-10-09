20418a38ef96c429d546d9284a8b8dbf8fe78b97
Pradeep4851 <mallelapradeep4851@gmail.com>
2024-10-09 13:10:00 +0530

    Updating changes #2

---
+++ Office365/src/main/java/com/office/model/EmailBody.java
@@ -0,0 +1,22 @@
+package com.office.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailBody {
+	
+	private String fromEmail;
+	private String toEmail;
+	private String subject;
+	private String messageBody;
+	
+
+}
